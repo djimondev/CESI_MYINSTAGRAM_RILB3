e795c8a7f460af8d538a61ecac622b7f496b30cc
djimondev <lefevre.djim@gmail.com@users.noreply.github.com>
2020-08-25 15:11:38 +0200

    start with stories, suggest and wall

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import Topbar from './components/Topbar';
 
 import './App.css';
+import Stories from './components/Stories';
+import Suggest from './components/Suggest';
+import Wall from './components/Wall';
 
 class App extends Component {
   render() {
     return (
-      <Topbar />
+      <>
+        <Topbar />
+        <div className="body--container">
+        <Suggest />
+        <Stories />
+        <Wall />
+        </div>
+      </>
     );
   }
 }
---
+++ src/components/Suggest.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Suggest extends Component {
+    render() {
+        return (
+            <>
+            </>
+        )
+    }
+}
+
+export default Suggest;
