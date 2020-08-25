7d3a40ac4ed9999879f5255a48ab26ba4d24f48f
djimondev <lefevre.djim@gmail.com@users.noreply.github.com>
2020-08-25 14:33:17 +0200

    push evertything

---
+++ src/components/Topbar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Topbar extends Component {
+    render() {
+        return (
+            <div>
+                <div className="logo--container">
+                    <img src="/assets/img/logo.png" srcset="/assets/img/logo-2x.png 2x" alt="" />
+                </div>
+                <div className="search--container">
+
+                </div>
+                <div className="navigation--container">
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Topbar;
